cc6bfc91f2f0d541ad7c0efc17f9d8d01f89ac8e
Documenter.jl <documenter@juliadocs.github.io>
2024-07-25 08:05:11 +0000

    build based on 56f0f4c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UnitfulData","category":"page"},{"location":"#UnitfulData","page":"Home","title":"UnitfulData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UnitfulData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UnitfulData]","category":"page"}]
+}
